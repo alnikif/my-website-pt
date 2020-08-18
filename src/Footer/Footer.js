e0d06aa7e3a2ae84f6369160faa6429ba13b7501
Oleksandr <nikiforiakoleksandr@gmail.com>
2020-08-18 13:50:30 +0200

    add footer

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -6,14 +6,33 @@ export default class Footer extends Component {
         return (
             <div className={styles.container}>
                 <div className={styles.form}>
-                <form action="">
-                    <input className={styles.inpt} type="number" placeholder='Жим лёжа'/>
-                    <input className={styles.inpt} type="text" placeholder='Приседания со штангой'/>
-                    <input className={styles.inpt} style={{height:'70px'}} type="text" placeholder='Укажите Ваш вес'/>
-                </form>
-                <button className={styles.btn} onClick={this.handleClick}>Готово!</button>
+                    <p>Напишите мне</p>
+                    <form action="">
+                        <input className={styles.inpt} type="email" placeholder='Эл. почта'/>
+                        <input className={styles.inpt} type="text" placeholder='Имя'/>
+                        <input className={styles.inpt} style={{height:'70px'}} type="text" placeholder='Сообщение'/>
+                    </form>
+                    <button className={styles.btn} onClick={this.handleClick}>Готово!</button>
                 </div>
-                <div className={styles.adress}></div>
+                <div className={styles.adress}>
+                    <div className={styles.location}>
+                        <p className ={styles.contactText}>
+                             <i class="fa fa-map-marker" aria-hidden="true"></i>
+                             Nowogrodzka 50/54, Warszawa, Polska
+                        </p>
+                    </div>
+                    <div className={styles.location}>
+                        <p className ={styles.contactText}>
+                            <i class="fa fa-phone" aria-hidden="true"></i>
+                            +48 883 973 602
+                        </p>
+                    </div>
+                
+        
+                    
+               
+
+                </div>
                
 
                 
